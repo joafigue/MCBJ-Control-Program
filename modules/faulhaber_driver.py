ce02de2f6f35724b25cb3bd7e4fcb57a8f56b9fd
joafigue <joafigue@gmail.com>
2017-01-25 15:55:40 -0300

    Comiting generated files for control loop prototype

--- modules/faulhaber_driver.py
+++ modules/faulhaber_driver.py
@@ -1,6 +1,6 @@
 # [[file:~/Lab_Diana/Programa_python/joaquin_rewrite/Measure_samples.org::*Faulhaber%20Driver][Faulhaber Driver:1]]
 #################################################################
-## @file    Faulhaber_driver.py
+## @file    faulhaber_driver.py
 #  @author  Joaquin Figueroa
 #  @date    Sun Sep 18, 2016
 #  @brief   Provides the interfaces for the Faulhaber motor
@@ -15,40 +15,22 @@
 #################################################################
 import visa
 import pylab as pl
+import parameters as param
 # Faulhaber Driver:1 ends here
-
-# [[file:~/Lab_Diana/Programa_python/joaquin_rewrite/Measure_samples.org::src-faulhaber-constants][src-faulhaber-constants]]
-#################################################################
-## @author  Joaquin Figueroa
-#  @brief   Constants used for the faulhaber interface
-#
-#  @details These are the constants used by the faulhaber
-#           interface.
-#################################################################
-class FH_CONST(object):
-    PORT = "COM3" #
-    BAUD_RATE = 9600 #
-    PITCH = 150 #
-    GEARBOX = 246 #
-    MAX_POS = 5000000#
-    MIN_POS = -7000000#
-    MAX_SPEED = 800#in RPM
-    MAX_ACCEL = 40#in RPM/s
-# src-faulhaber-constants ends here
 
 # [[file:~/Lab_Diana/Programa_python/joaquin_rewrite/Measure_samples.org::*Conversion%20functions][Conversion functions:1]]
 #################################################################
 ## @brief   Converts motor speed from um/s to rpm
 #################################################################
 def faulhaber_convert_ums_to_rpm(ums):
-    rpm = 60 * FH_CONST.GEARBOX * (float(ums) / FH_CONST.PITCH)
+    rpm = 60 * param.FH_CONST.GEARBOX * (float(ums) / param.FH_CONST.PITCH)
     return rpm
 
 #################################################################
 ## @brief   Converts motor speed from rpm to um/s
 #################################################################
 def faulhaber_convert_rpm_to_ums(rpm):
-    ums = (rpm * FH_CONST.PITCH)/( 60 * FH_CONST.GEARBOX)
+    ums = (rpm * param.FH_CONST.PITCH)/( 60 * param.FH_CONST.GEARBOX)
     return ums
 # Conversion functions:1 ends here
 
@@ -92,13 +74,86 @@ class faulhaber_motor(object):
         self._set_max_speed_rpm()
         self._set_max_acceleration()
         self.disable_motor()
+    
     # [[file:~/Lab_Diana/Programa_python/joaquin_rewrite/Measure_samples.org::faulhaber-basic-fn-interface][faulhaber-basic-fn-interface]]
     #################################################################
     ## @brief   Reset controles
     #################################################################
-    def _reset_controller(self): #(ref:fh-basic-fn-enable)
-        self.motor_ctrl = visa.SerialInstrument(FH_CONST.PORT)
-        self.motor_ctrl.baud_rate = FH_CONST.BAUD_RATE
+    def _reset_controller(self): #(ref:fh-internal-fn-enable)
+        self.motor_ctrl = visa.SerialInstrument(param.FH_CONST.PORT)
+        self.motor_ctrl.baud_rate = param.FH_CONST.BAUD_RATE
+    
+    #################################################################
+    ## @brief   Ensures that the motor honor the user defined limits
+    #################################################################
+    def _set_respect_user_limits(self):#(ref:fh-internal-fn-user-lim)
+        self.motor_ctrl.ask("APL 1")
+    
+    #################################################################
+    ## @brief   Set the maximum and minimum position for the motor axis
+    #################################################################
+    def _set_pos_range(self):#(ref:fh-internal-fn-pos-range)
+        max_pos_str = "LL %d" % param.FH_CONST.MAX_POS
+        min_pos_str = "LL %d" % param.FH_CONST.MIN_POS
+        self.motor_ctrl.ask(max_pos_str)
+        self.motor_ctrl.ask(min_pos_str)
+    #################################################################
+    ## @brief   Sets the maximum speed for the motor
+    #################################################################
+    def _set_max_speed_rpm(self):#(ref:fh-internal-fn-max-speed-rpm)
+        max_speed_str = "SP %d" %param.FH_CONST.MAX_SPEED
+        self.motor_ctrl.ask(max_speed_str)
+    
+    #################################################################
+    ## @brief   Sets the maximum acceleration for the motor
+    #################################################################
+    def _set_max_acceleration(self):#(ref:fh-internal-fn-max-accel)
+        max_accel_str = "SP %d" %param.FH_CONST.MAX_ACCEL
+        self.motor_ctrl.ask(max_accel_str)
+    
+    #################################################################
+    ## @brief   Sets the target speed of the motor in rpm
+    #################################################################
+    def _set_target_speed_rpm(self, speed):#(ref:fh-internal-fn-speed-rpm)
+        target_speed_str = "v %1.2f" %speed
+        self.state.update_current_speed(speed)
+        self.motor_ctrl.ask(target_speed_str)
+    
+    #################################################################
+    ## @brief   Returns the current axis position
+    #  @Note    Not to be used directly in other parts of the program
+    #################################################################
+    def _query_current_axis_position(self, count=0):#(ref:fh-internal-fn-axis-pos)
+        pos = self.motor_ctrl.ask("pos")
+        try:
+            pos = int(pos)
+        except:
+            if count < 2:
+                self._reset_controller()
+                pl.pause(0.02)
+                return self._query_current_axis_position(count +1)
+            print("Error, Returned position '{0}' was not a valid int".format(pos))
+        return pos
+    
+    #################################################################
+    ## @brief   Initializes the motor state
+    #################################################################
+    def _init_state(self):#(ref:fh-internal-fn-init-state)
+        start_pos = self._query_current_axis_position()
+        self.state = faulhaber_state(start_pos, 0)
+    
+    #################################################################
+    ## @brief   Moves the motor for a small time in the direction set by speed
+    #################################################################
+    def _small_move_with_speed(self, speed):
+        self.enable_motor()
+        self.set_target_speed(speed)
+        pl.pause(0.5)
+        self.stop_motor()
+        pl.pause(0.5)
+        self.disable_motor()
+    # faulhaber-basic-fn-interface ends here
+    # [[file:~/Lab_Diana/Programa_python/joaquin_rewrite/Measure_samples.org::faulhaber-basic-fn-interface][faulhaber-basic-fn-interface]]
     
     #################################################################
     ## @brief   Enables the motor
@@ -113,63 +168,11 @@ class faulhaber_motor(object):
         self.motor_ctrl.ask("di")
     
     #################################################################
-    ## @brief   Ensures that the motor honor the user defined limits
-    #################################################################
-    def _set_respect_user_limits(self):#(ref:fh-basic-fn-user-lim)
-        self.motor_ctrl.ask("APL 1")
-    
-    #################################################################
-    ## @brief   Set the maximum and minimum position for the motor axis
-    #################################################################
-    def _set_pos_range(self):#(ref:fh-basic-fn-pos-range)
-        max_pos_str = "LL %d" % FH_CONST.MAX_POS
-        min_pos_str = "LL %d" % FH_CONST.MIN_POS
-        self.motor_ctrl.ask(max_pos_str)
-        self.motor_ctrl.ask(min_pos_str)
-    #################################################################
-    ## @brief   Sets the maximum speed for the motor
-    #################################################################
-    def _set_max_speed_rpm(self):#(ref:fh-basic-fn-max-speed-rpm)
-        max_speed_str = "SP %d" %FH_CONST.MAX_SPEED
-        self.motor_ctrl.ask(max_speed_str)
-    
-    #################################################################
-    ## @brief   Sets the maximum acceleration for the motor
-    #################################################################
-    def _set_max_acceleration(self):#(ref:fh-basic-fn-max-accel)
-        max_accel_str = "SP %d" %FH_CONST.MAX_ACCEL
-        self.motor_ctrl.ask(max_accel_str)
-    
-    #################################################################
-    ## @brief   Sets the target speed of the motor in rpm
-    #################################################################
-    def _set_target_speed_rpm(self, speed):#(ref:fh-basic-fn-speed-rpm)
-        target_speed_str = "v %1.2f" %speed
-        self.state.update_current_speed(speed)
-        self.motor_ctrl.ask(target_speed_str)
-    
-    #################################################################
     ## @brief   Sets the target speed of the motor in us
     #################################################################
     def set_target_speed(self, speed_us):#(ref:fh-basic-fn-speed-us)
         speed_rpm = faulhaber_convert_ums_to_rpm(speed_us)
         self._set_target_speed_rpm(speed_rpm)
-    
-    #################################################################
-    ## @brief   Returns the current axis position
-    #  @Note    Not to be used directly in other parts of the program
-    #################################################################
-    def _query_current_axis_position(self, count=0):#(ref:fh-basic-fn-axis-pos)
-        pos = self.motor_ctrl.ask("pos")
-        try:
-            pos = int(pos)
-        except:
-            if count < 2:
-                self._reset_motor_ctrl()
-                pl.pause(0.02)
-                return self._query_current_axis_position(count +1)
-            print("Error, Returned position '{0}' was not a valid int".format(pos))
-        return pos
     
     #################################################################
     ## @brief   Returns the current axis position, updates the state
@@ -179,20 +182,23 @@ class faulhaber_motor(object):
         self.state.update_position(pos)
         return pos
     
-    #################################################################
-    ## @brief   Initializes the motor state
-    #################################################################
-    def _init_state(self):#(ref:fh-basic-fn-init-state)
-        start_pos = self._query_current_axis_position()
-        self.state = faulhaber_state(start_pos, 0)
-    # faulhaber-basic-fn-interface ends here
-    # [[file:~/Lab_Diana/Programa_python/joaquin_rewrite/Measure_samples.org::faulhaber-real-fn-interface][faulhaber-real-fn-interface]]
     def stop_motor(self):
         self.set_target_speed(0)
     
     def is_stopped(self):
         return self.state.is_motor_stopped()
     
+    def small_break(self, speed=-1.0):
+        real_speed=-1*abs(speed)
+        self._small_move_with_speed(real_speed)
+    
+    
+    def small_make(self, speed=1.0):
+        real_speed=abs(speed)
+        self._small_move_with_speed(real_speed)
+    
+    # faulhaber-basic-fn-interface ends here
+    # [[file:~/Lab_Diana/Programa_python/joaquin_rewrite/Measure_samples.org::faulhaber-advanced-fn-interface][faulhaber-advanced-fn-interface]]
     
     ## Manual Controls
     def set_motor_pos(self, new_pos):
@@ -225,5 +231,5 @@ class faulhaber_motor(object):
             print("Current Position = {0}".format(position))
         print("Final Position = {0}".format(self.get_position()))
         self.disable_motor()
-    # faulhaber-real-fn-interface ends here
+    # faulhaber-advanced-fn-interface ends here
 # faulhaber-class-def ends here
